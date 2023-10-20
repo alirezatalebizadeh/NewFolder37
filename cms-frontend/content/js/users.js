ea64afebc785acdec3c77c3130d7e83f72f6b8f2
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-10-20 18:10:58 +0330

    coding js to edit user Info

--- cms-frontend/content/js/users.js
+++ cms-frontend/content/js/users.js
@@ -2,12 +2,16 @@ let usersContainer = document.querySelector('.users-wrap')
 let removeModalContainer = document.querySelector('.remove_modal')
 let removeUserBtn = document.querySelector('.accept-btn')
 let cancelDeleteBtn = document.querySelector('.unaccept-btn')
+let editBtn = document.querySelector('.edit_btn')
+let userNameInput = document.querySelector('#username-input')
+let firstNameInput = document.querySelector('#first-name-input')
+let lastNameInput = document.querySelector('#last-name-input')
 let mainUserID = null;
 
 
 
 
-
+//! fetch all user from db
 function getAllUsers() {
   usersContainer.innerHTML = ''
   fetch('http://localhost:3000/api/users/')
@@ -36,9 +40,9 @@ function getAllUsers() {
 
               <div class="user-btns-group">
                 <!--  --- edit btn ---- ! -->
-                <button class="user-edit-btn">edit</button>
+                <button class="user-edit-btn" onclick='showEditModal("${user._id}")'>edit</button>
                 <!--  -- remove btn --- ! -->
-                <button class="user-remove-btn" onclick='showModal("${user._id}")'>remove</button>
+                <button class="user-remove-btn" onclick='showRemoveModal("${user._id}")'>remove</button>
               </div>
             </div>
               `)
@@ -47,30 +51,84 @@ function getAllUsers() {
 }
 
 //! show remove modal
-function showModal(userID) {
+function showRemoveModal(userID) {
   mainUserID = userID;
   removeModalContainer.classList.add('visible')
+  document.querySelector('.deleteModal').style.display = 'block';
+  document.querySelector('.editModal').style.display = 'none';
 }
 
-//! close remove modal
-function closeRemoveModal() {
+//! show edit modal
+function showEditModal(userID) {
+  mainUserID = userID;
+  removeModalContainer.classList.add('visible')
+  document.querySelector('.deleteModal').style.display = 'none';
+  document.querySelector('.editModal').style.display = 'block';
+}
+
+//! close  modal
+function closeModal() {
   removeModalContainer.classList.remove('visible')
-
 }
 
 
 
-
+//! remove user from db
 removeUserBtn.addEventListener('click', () => {
   fetch(`http://localhost:3000/api/users/${mainUserID}`, {
     method: 'DELETE'
   })
     .then(res => {
       console.log(res)
-      closeRemoveModal()
+      closeModal()
       getAllUsers()
     })
 })
 
+
+
 window.addEventListener('load', getAllUsers)
-cancelDeleteBtn.addEventListener('click', closeRemoveModal)
+cancelDeleteBtn.addEventListener('click', closeModal)
+
+
+//!Edit User Info
+editBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let userUpdateInfo = {
+    firstName: firstNameInput.value.trim(),
+    lastName: lastNameInput.value.trim(),
+    userName: userNameInput.value.trim(),
+    profile: 'content/img/profile/banana.png',
+  }
+  console.log(userUpdateInfo);
+
+
+  fetch(`http://localhost:3000/api/users/${mainUserID}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userUpdateInfo)
+  })
+    .then(res => {
+      console.log(res);
+      closeModal()
+      clearInputsEdit()
+      getAllUsers()
+    })
+
+})
+
+function clearInputsEdit() {
+  firstNameInput.value = ''
+  lastNameInput.value = ''
+  userNameInput.value = ''
+
+}
+
+// document.body.addEventListener('click', (e) => {
+//   if (e.target.class !== 'modal-content') {
+//     console.log(true);
+//   }
+// })
